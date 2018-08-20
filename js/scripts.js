0fbebb8fcec1c822b2541b3178dc1bbaeef3401a
Meguru Yasuda and Victoria Oh <victoria@>
2018-08-20 16:34:53 -0700

    clear input form when hit submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,25 @@
+// business logic
+function ToDo(task) {
+  this.task = task;
+}
+
 // user interface
 $(document).ready(function() {
   $("#task-form").submit(function(event){
     event.preventDefault();
-
-    function ToDo(task) {
-      this.task = task;
-    }
-
     var userInput = $("#new-task").val();
     var newToDo = new ToDo(userInput);
 
     $("#todo-list").append("<li class='task'>" + newToDo.task + "</li>");
+    $("input#new-task").val("");
 
     $(".task").last().click(function() {
       $("#done-list").append("<li class='task'>" + newToDo.task + "</li>");
       $(this).hide(newToDo.task);
+
+       //what is this for?? we tested and it seemed it din't do anything.
     });
 
   });
-
-
-
-
-
-
-
-
 
 });
