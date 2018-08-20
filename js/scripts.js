12f5b29c76af86ee425495d0bbfee054e5ad00d3
Meguru Yasuda and Victoria Oh <meguru@>
2018-08-20 16:18:19 -0700

    add functionality to remove todo task to done list

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,29 @@
 $(document).ready(function() {
   $("#task-form").submit(function(event){
     event.preventDefault();
+
+    function ToDo(task) {
+      this.task = task;
+    }
+
     var userInput = $("#new-task").val();
-    $("#todo-list").append("<li>" + userInput + "</li>");
+    var newToDo = new ToDo(userInput);
+
+    $("#todo-list").append("<li class='task'>" + newToDo.task + "</li>");
+
+    $(".task").last().click(function() {
+      $("#done-list").append("<li class='task'>" + newToDo.task + "</li>");
+      $(this).hide(newToDo.task);
+    });
+
   });
+
+
+
+
+
+
+
+
+
 });
